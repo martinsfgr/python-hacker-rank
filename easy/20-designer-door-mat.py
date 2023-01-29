d22e3c991ab7fb439d479bad2dd840c041f66283
Mauricio <mauriciofagner2010@hotmail.com>
2023-01-29 03:20:56 -0300

    Adicionar desafio 20

---
+++ easy/20-designer-door-mat.py
@@ -0,0 +1,26 @@
+N, M = map(int, input().split())
+
+center = N // 2
+output = []
+
+
+for i in range(N):
+    if i == center:
+        line_to_print = "WELCOME".center(M, "-")
+        output.append(line_to_print)
+
+    elif i > center:
+        line_to_print = output[:center]
+        line_to_print.reverse()
+
+        for j in line_to_print:
+            output.append(j)
+        break
+
+    else:
+        line_to_print = (".|." * (i * 2 + 1)).center(M, "-")
+        output.append(line_to_print)
+    
+
+for line in output:
+    print(line)
